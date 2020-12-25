cb67e6386bf736bfc2c5cedbeeca6e5e955a0922
mrchanan <t0829565356@gmail.com>
2020-12-25 22:49:08 +0700

    added index file

---
+++ test1.py
@@ -0,0 +1,32 @@
+import turtle
+s = turtle.getscreen()
+t = turtle.Turtle()
+turtle.bgcolor("black")
+
+def S(x):
+    t.speed(x)
+S(1000)
+
+def cir(x):
+    t.circle(x)
+def col(y):
+    t.pencolor(y)
+color = ['yellow','blue','green','red']
+
+
+'''
+for c in color:
+    col(c)
+    for i in range(360):
+        cir(i)
+        t.lt(i)
+  '''  
+
+
+
+for i in range(360):
+    for c in color:
+        col(c)
+        cir(i)
+        t.lt(i)
+
